e9bc4cf6933b0190ceac16b283573d59440d9cbc
Leto <leto@fondation.org>
2024-04-19 08:05:44 +0200

    adding Neorg overlay

--- flake.nix
+++ flake.nix
@@ -1,15 +1,36 @@
 {
-  description = "A very basic flake";
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    norg.url = "github:nvim-neorg/tree-sitter-norg/dev";
+    norg-meta.url = "github:nvim-neorg/tree-sitter-norg-meta";
+
+    neorg = {
+      url = "github:nvim-neorg/neorg";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    norg,
+    norg-meta,
+    neorg,
+  }: {
+    overlays.default = final: prev: let
+      inherit (final.lib) attrValues elemt filter filterAttrs isDerivation;
+    in {
+      vimPlugins = prev.vimPlugins.extend (f: p: {
+        neorg = final.vimUtils.buildVimPlugin {
+          pname = "neorg";
+          version = "v7.0.0";
+          src = neorg;
+        };
+      });
+    };
   };
 }
